65c76bee14f5006501af6360de2c0e00b03c9dde
Not_staff <108166164+staFF6773@users.noreply.github.com>
2025-05-03 12:47:22 -0600

    layout.tsx | Se añadio el @/components/LoadingScreen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { LanguageProvider } from '@/lib/LanguageContext';
+import LoadingScreen from '@/components/LoadingScreen';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <LanguageProvider>
+          <LoadingScreen />
           {children}
         </LanguageProvider>
       </body>
