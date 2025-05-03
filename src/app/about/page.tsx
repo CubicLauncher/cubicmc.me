1498483c4f0c621a92b2312d6be006df71ab3939
Not_staff <108166164+staFF6773@users.noreply.github.com>
2025-05-03 00:56:15 -0600

    page.tsx | pagina de informacion

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import About from '@/components/about';
+import Hero from '@/components/hero';
+import Nav from '@/components/nav';
+import Footer from '@/components/footer';
+
+export default function AboutPage() {
+    return <About />;
+} 
